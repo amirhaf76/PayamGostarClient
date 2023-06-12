2cf5e5a5e63c9e8f4f02ce0c64fba95a46c0f805
Amirhaf76 <44908316+amirhaf76@users.noreply.github.com>
2023-06-12 12:50:02 +0330

    Test for them are updated.

--- PayamGostarClientTest/UnitTest1.cs
+++ PayamGostarClientTest/UnitTest1.cs
@@ -1,4 +1,6 @@
-using PayamGostarClient.CrmObjectModelInitServiceModels;
+﻿using PayamGostarClient.CrmObjectModelInitServiceModels;
+using PayamGostarClient.CrmObjectModelInitServiceModels.CrmObjectModels;
+using PayamGostarClient.CrmObjectModelInitServiceModels.CrmObjectModels.CrmObjectTypeModels;
 using PayamGostarClient.CrmObjectModelInitServiceModels.ServiceModels;
 using System.Threading.Tasks;
 using Xunit;
@@ -8,6 +10,10 @@ namespace PayamGostarClientTest
 {
     public class UnitTest1
     {
+        private const string URL = "http://192.168.11.9/";
+        //private const string URL = "https://webhook.site/eba279bf-0642-4288-9a82-b4e8d15fd276";
+        private const string LANGUAGE_CULTURE = "Fa";
+
         private readonly ITestOutputHelper _testOutput;
 
         public UnitTest1(ITestOutputHelper testOutput)
@@ -16,20 +22,41 @@ namespace PayamGostarClientTest
         }
 
         [Fact]
-        public async Task Test1()
+        public async Task InitAsync_EmptyModel_DoNothing()
         {
             var initServiceConfig = new CrmObjectModelInitServiceConfig
             {
                 ClientService = new PayamGostarClient.ApiServices.PayamGostarClientServiceConfig
                 {
-                    Url = "http://192.168.11.9/",
-                    LanguageCulture = "Fa"
+                    Url = URL,
+                    LanguageCulture = LANGUAGE_CULTURE,
                 }
             };
 
             var crmModelService = new CrmObjectModelInitService(initServiceConfig);
 
             await crmModelService.InitAsync();
+        }
+
+        [Fact]
+        public async Task InitAsync_FormModel_DoNothing()
+        {
+            var initServiceConfig = new CrmObjectModelInitServiceConfig
+            {
+                ClientService = new PayamGostarClient.ApiServices.PayamGostarClientServiceConfig
+                {
+                    Url = URL,
+                    LanguageCulture = LANGUAGE_CULTURE,
+                    JwToken = AdminJwToken.JWTOKEN,
+                }
+            };
+
+            var crmModelService = new CrmObjectModelInitService(initServiceConfig);
+
+            await crmModelService.InitAsync(new CrmFormModel
+            {
+                Name = new ResourceValue[] { new ResourceValue { Value = "تست", LanguageCulture = LANGUAGE_CULTURE } },
+            });
         }
     }
 
