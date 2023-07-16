edc3c6d825fec69b68f2d513903f98d7aff27905
Amirhaf76 <44908316+amirhaf76@users.noreply.github.com>
2023-07-16 16:36:30 +0330

    Adding product group interface, extension and dtos.

---
+++ PayamGostarClient/ApiClient/Abstractions/Customization/Product/IPayamGostarProductGroupApiClient.cs
@@ -0,0 +1,16 @@
+﻿using PayamGostarClient.ApiClient.Dtos.ProductDtos.Create;
+using PayamGostarClient.ApiClient.Dtos.ProductDtos.Get;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayamGostarClient.ApiClient.Abstractions.Customization.Product
+{
+    public interface IPayamGostarProductGroupApiClient
+    {
+        Task<IEnumerable<ProductGroupGetResponseDto>> GetAsync(ProductGroupGetRequestDto request);
+
+        Task<IEnumerable<ProductGroupCreationResponseDto>> CreateAsync(ProductGroupCreationRequestDto request);
+    }
+}
---
+++ PayamGostarClient/ApiClient/Extension/ProductGroupApiClientExtension.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PayamGostarClient.ApiClient.Extension
+{
+    internal static class ProductGroupApiClientExtension
+    {
+    }
+}
