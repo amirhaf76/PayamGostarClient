c341027fdfe07de9756ff7c0cd6effeb0c3bb644
Amirhaf76 <44908316+amirhaf76@users.noreply.github.com>
2023-07-16 16:26:31 +0330

    First parts of product group init service  are implemented.

---
+++ PayamGostarClient/Initializer/CrmModels/CrmObjectTypeGeneralModels/ProductGroupModel.cs
@@ -0,0 +1,15 @@
+﻿using PayamGostarClient.Initializer.Abstractions.CrmModel;
+using PayamGostarClient.Initializer.Enums;
+using System;
+
+namespace PayamGostarClient.Initializer.CrmModels.CrmObjectTypeGeneralModels
+{
+    public class ProductGroupModel : ICustomizationCrmModel
+    {
+        public string Name { get; set; }
+
+        public Guid? ParentId { get; set; }
+
+        public CustomizationCrmType CustomizationCrmType => CustomizationCrmType.ProductGroup;
+    }
+}
---
+++ PayamGostarClient/Initializer/Services/ProductGroupInitService.cs
@@ -0,0 +1,49 @@
+﻿using PayamGostarClient.ApiClient.Abstractions;
+using PayamGostarClient.Initializer.Abstractions.InitServices;
+using PayamGostarClient.Initializer.CrmModels.CrmObjectTypeGeneralModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PayamGostarClient.Initializer.Services
+{
+    public class ProductGroupInitService : IInitService
+    {
+        private readonly ProductGroupModel _productGroupModel;
+
+
+        public ProductGroupInitService(ProductGroupModel productGroupModel, IPayamGostarApiClient payamGostarApiClient)
+        {
+            _productGroupModel = productGroupModel;
+        }
+
+
+        public async Task<bool> CheckExistenceSchemaAsync()
+        {
+            var searchingResult = await SearchProductGroupAsync();
+
+            return searchingResult.Any();
+        }
+
+        public async Task InitAsync()
+        {
+            var searchingResult = await SearchProductGroupAsync();
+
+            if (!searchingResult.Any())
+            {
+                await CreateProductGroupAsync();
+            }
+        }
+
+
+        private Task<IEnumerable<object>> SearchProductGroupAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private Task<object> CreateProductGroupAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
